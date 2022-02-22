5b419fb48318f7ef51be967d5bf5c7115d1b3302
Antoan <aachavdarov93@gmail.com>
2022-02-22 18:29:47 +0300

    asdasdasd

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <div>
         <h1>CS:GO Buy Binds Generator</h1>
-        <img src="../weapons.jpg" alt="weapons" />
+        <img src="../public/weapons" alt="weapons" />
     </div>
   )
 }
